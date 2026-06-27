e3c57dd7bd92417cb1be90d8ebc9db1010766b06
محمد يحيى <mrx7zthe@gmail.com>
2026-06-28 01:25:41 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+links.forEach(l=>l.classList.remove("active"));
+
+link.classList.add("active");
+
+});
+
+});
